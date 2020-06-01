8a7d12576f0d82af067feee2d623d4353316c8df
Nur Oktaviani <65939535+nurokt@users.noreply.github.com>
2020-06-01 20:02:03 +0700

    Update perhitungan_tagihan_pulsa2.py

--- perhitungan_tagihan_pulsa2.py
+++ perhitungan_tagihan_pulsa2.py
@@ -39,3 +39,15 @@ print(data)
 
 print("--------------------------------------------------------------------------------")
 print('Minggu ke-\t Jumlah SMS\t Menit telepon ke sesama operator(menit)\t Menit telepon ke operaator lain(menit)\t Total pulsa (Rp)\t Kuota sosmed (kb)\t Kuota lainnya (kb)\t Tagihan kuota terpakai (Rp)\t Tagihan/minggu')
+
+for i in range(0,len(data)):
+    print(data[i][0],for i in range(0,len(data)):
+    print(data[i][0],'\t', data[i][1], '\t',data[i][2], '\t',data[i][3], '\t',data[i][4], '\t',data[i][5], '\t',data[i][6], '\t',data[i][7], '\t',data[i][8])
+    i += 1
+
+print("--------------------------------------------------------------------------------")
+   
+with open(nama_file, 'w') as outf:
+    header = ['Minggu ke-','Jumlah SMS','Menit telepon ke sesama operator(menit)','Menit telepon ke operaator lain(menit)','Total pulsa (Rp)','Kuota sosmed (kb)','Kuota lainnya (kb)','Tagihan kuota terpakai (Rp)','Tagihan/minggu']
+    writer = csv.DictWriter(outf, fieldnames=header)
+    writer.writeheader()
